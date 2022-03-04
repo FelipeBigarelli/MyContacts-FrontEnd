afc7cd1a0f43a406720e9f2d90a91ab2fa6fd535
FelipeBigarelli <felipebigarelli@hotmail.com>
2022-03-04 03:22:56 -0300

    PageHeader

---
+++ src/pages/Home/index.js
@@ -0,0 +1,91 @@
+import {
+  Container, Header, ListContainer, Card, InputSearchContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function NewContact() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquise pelo nome..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Felipe Bigarelli</strong>
+              <small>instagram</small>
+            </div>
+            <span>felipebigarelli@hotmail.com</span>
+            <span>(61) 98171-2059</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Felipe Bigarelli</strong>
+              <small>instagram</small>
+            </div>
+            <span>felipebigarelli@hotmail.com</span>
+            <span>(61) 98171-2059</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Felipe Bigarelli</strong>
+              <small>instagram</small>
+            </div>
+            <span>felipebigarelli@hotmail.com</span>
+            <span>(61) 98171-2059</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
+}
